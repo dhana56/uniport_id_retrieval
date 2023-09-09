01715cf1a62319c8deb076f4f5fac0384e57cf9b
dhana <dhanasekharvk@gmail.com>
2023-09-09 20:38:58 +0530

    v1

---
+++ fun.py
@@ -0,0 +1,58 @@
+#Importing the required libraries 
+from pathlib import Path
+import pickle
+
+
+#Processing file function
+def text_procs(file):
+    """Function return pdb_id as list from the file
+    :File, file path
+    """
+
+    path_1 = Path(r"entry_file_error.txt")
+    if path_1.is_file()==True:
+        path_1.unlink()
+    else:
+        pass
+
+    data_lis = []
+    for i in open(file):
+        if len(i.strip())==4 :
+            data_lis.append(i.strip())
+        else:
+            with open(r"entry_file_error.txt", 'a') as file:
+                file.write(i + '\n')
+                file.close()
+    return data_lis
+
+#Saving the Output file:
+
+def file_save(x):
+    """Function create json file
+    : x, dictionary as a input
+    """
+    with open("result.pickle",'wb') as file:
+        pickle.dump(x,file)
+
+def pickle_read():
+    """Function return the entry based uniport details"""
+
+    with open("result.pickle", "rb") as pickle_file:
+        loaded_data = pickle.load(pickle_file)
+    entry = input("Please enter the PDB id: ")
+    return loaded_data[entry.lower()]
+
+def save_data():
+    "Function that save the data in text file"
+
+    with open("result.pickle",'rb') as file:
+        loaded = pickle.load(file)
+    
+    with open("result_output.txt",'w') as file_1:
+        file_1.write(str(loaded))
+
+        
+    
+
+            
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import fun
+import prod_cod
+
+if __name__ == "__main__":
+    entry = input("Please provide the PDB_id or file path: ")
+    print_or_save = input("save or print ? ")
+
+    if Path(entry).is_file()==True:
+        entry_lis = fun.text_procs(entry)
+    elif (len(entry)==4
+            and entry[0].isnumeric()==True):
+        entry_lis= [entry]
+    else:
+        entry_lis = []
+        print("Please provide a valid entry")
+
+    out_put = prod_cod.scrappy_fun(entry_lis)
+    if print_or_save.lower()=='print':
+        all_one = input("all or one: ")
+        if all_one.lower() == "all":
+            print(out_put)
+        else:
+            fun.file_save(out_put)
+            print(fun.pickle_read())
+    else:
+        print("Please wait processing and saving your queries in a file")
+        fun.file_save(out_put)
+        fun.save_data()
+
+    
+
+
+        
